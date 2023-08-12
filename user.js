8f07e7fbecf80efc2417c9fdc2d18ccf4467982b
Reborn Rulz <77287878+rebornrulz@users.noreply.github.com>
2023-08-13 01:46:36 +0800

    Create user.js

---
+++ user.js
@@ -0,0 +1,15 @@
+{
+    "id": "mem_04cf14cafa0111d432c1a2797aa30a56632cbc15",
+    "auth": {
+        "isVerified": true,
+        "hasPassword": false,
+        "signInMethod": "github",
+        "email": "R3b0rnrulz@gmail.com",
+        "providerUserId": "77287878"
+    },
+    "name": "Reborn Rulz",
+    "customFields": {},
+    "metadata": {},
+    "createdAtMs": 1691861797784,
+    "updatedAtMs": 1691861797784
+}
